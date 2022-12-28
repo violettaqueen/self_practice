f69abb3e773f43d89d069df49041c6b5023c0c20
Violetta <violetta.v.queen@gmail.com>
2022-12-27 17:52:08 -0700

    sunset view practice

--- src/sunsetViews/SunsetView2.java
+++ src/sunsetViews/SunsetView2.java
@@ -1,6 +1,7 @@
 package sunsetViews;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Stack;
 
@@ -8,19 +9,28 @@ public class SunsetView2 {
 
     public static void main(String[] args) {
         int[] buildings= {2,1,3,1};
-        System.out.println(sunsetView(buildings));
+        System.out.println(sunsetView(buildings, "west"));
     }
-    public static List<Integer> sunsetView(int[] buildings){
+    public static List<Integer> sunsetView(int[] buildings, String direction){
 
         Stack<Integer> stack = new Stack<>();
+        int i = 0;  //default values
+        int step = 1;
 
-        for (int i = 0; i < buildings.length; i++) {
+        if (direction.equalsIgnoreCase("west")){
+            i = buildings.length-1;
+            step =-1;
+        }
+
+        while (i >= 0 && i < buildings.length) {
 
             while (!stack.isEmpty() && buildings[i] >= buildings[stack.peek()]){
                 stack.pop();
             }
             stack.push(i);
+            i+=step;
         }
+        if (direction.equalsIgnoreCase("west")) Collections.reverse(stack);
         return new ArrayList<>(stack);
 
 
