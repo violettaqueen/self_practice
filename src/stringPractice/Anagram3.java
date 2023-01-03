77eb6256cc991173679ac28bc7f847a7a089049f
Violetta <violetta.v.queen@gmail.com>
2023-01-02 20:33:00 -0700

    anagram practice

---
+++ src/stringPractice/Anagram3.java
@@ -0,0 +1,23 @@
+package stringPractice;
+
+import java.util.Arrays;
+
+public class Anagram3 {
+
+    public boolean isAnagram(char[] s1, char[] s2) {
+
+        if (s1.length != s2.length) return false;
+        Arrays.sort(s1);
+        Arrays.sort(s2);
+
+        for (int i = 0; i < s1.length; i++) {
+            if (s1[i] != s2[i])
+                return false;
+        }
+        return true;
+    }
+
+}
+
+
+
