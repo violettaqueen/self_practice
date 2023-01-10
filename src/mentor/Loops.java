b5c5c054621eef5214907a94e6f0bb8d4bb04d26
Violetta <violetta.v.queen@gmail.com>
2023-01-09 21:00:24 -0700

    practice

---
+++ src/mentor/Loops.java
@@ -0,0 +1,29 @@
+package mentor;
+
+public class Loops {
+    public static void main(String[] args) {
+
+        System.out.println("TRIANGLE");
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println("SQUARE");
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println("INVERTED TRIANGLE");
+        for (int i = 5; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+
+        }
+    }
+}
