332c2b51cbe09733cddb7850600b7f3707ab74ed
Violetta <violetta.v.queen@gmail.com>
2023-01-09 21:00:05 -0700

    practice

--- src/leetcode/PalindromeNumber9.java
+++ src/leetcode/PalindromeNumber9.java
@@ -4,14 +4,26 @@ public class PalindromeNumber9 {
 
     public static void main(String[] args) {
 
-        int x = 121;
+        int x = -121;
 
-
+        System.out.println(isPalindrome(x));
     }
 
-//    public static boolean isPalindrome(int num){
-//
-//
-//
-//    }
+    public static boolean isPalindrome(int num) {
+
+        String s = String.valueOf(num);
+        int p1 = 0;
+        int p2 = s.length() - 1;
+
+        while (p1 <= p2) {
+            if (s.charAt(p1) != s.charAt(p2)) {
+                return false;
+
+            }
+            p1++;
+            p2--;
+        }
+        return true;
+
+    }
 }
