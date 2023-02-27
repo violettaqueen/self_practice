68d3240026f8ac40dce75e31cdd9953111d2e924
Violetta <violetta.v.queen@gmail.com>
2023-02-27 06:20:37 -0700

    fibonacci

---
+++ src/numbers/Fibonacci.java
@@ -0,0 +1,24 @@
+package numbers;
+
+public class Fibonacci {
+
+    public static void main(String[] args) {
+
+        int num = 21;
+        allFib(21);
+
+
+    }
+    public static int fibonacci(int n){
+
+        if (n < 0) return 0;
+        if (n==1) return 1;
+        else return fibonacci(n-1) + fibonacci(n-2);
+    }
+    public static void allFib(int n){
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(i+":"+ fibonacci(i)+" ");
+        }
+    }
+}
